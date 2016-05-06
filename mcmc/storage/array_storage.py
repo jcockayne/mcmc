373d6b0ab108dc9b4a3a3864b121d3393109dd60
Jon Cockayne <benorn@gmail.com>
2016-05-06 16:51:29 +0100

    Support for thinning in array_storage

--- mcmc/storage/array_storage.py
+++ mcmc/storage/array_storage.py
@@ -1,12 +1,24 @@
 import numpy as np
+import logging
+logger = logging.getLogger(__name__)
+
+
 class ArrayStorage(object):
-    def __init__(self, arr_size):
-        self.__array__ = np.empty(arr_size)
+    def __init__(self, total_samples, variates , thin=1):
+        self.__array__ = np.empty((int(total_samples / thin), variates))
         self.__current_index__ = 0
+        self.__real_index__ = 0
+        self.__thin__ = thin
 
     def add_sample(self, item):
-        self.__array__[self.__current_index__, :] = item
-        self.__current_index__ += 1
+        if self.__real_index__ % self.__thin__ == 0:
+            if self.__current_index__ >= self.__array__.shape[0]:
+                logger.warn('Trying to write to an index outside of the bounds of the array!')
+            else:
+                self.__array__[self.__current_index__, :] = item
+                self.__current_index__ += 1
+
+        self.__real_index__ += 1
 
     @property
     def array(self):
