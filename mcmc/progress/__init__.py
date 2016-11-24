378061239bd61588ed81830444ec056869e638ae
Jon Cockayne <benorn@gmail.com>
2016-11-24 10:39:13 +0000

    Corrected a bug in progress

--- mcmc/progress/__init__.py
+++ mcmc/progress/__init__.py
@@ -1,7 +1,11 @@
 import jupyter_progress, stdout_progress, null_progress, base
 
-notebook = False
 verbose = True
+PROGRESS_NOTEBOOK = 'notebook'
+PROGRESS_STDOUT = 'stdout'
+PROGRESS_NONE = 'none'
+
+default_progress = PROGRESS_STDOUT
 
 
 def factory(obj):
@@ -9,22 +13,14 @@ def factory(obj):
         return get_default_progress()
     if isinstance(obj, base.ProgressBase):
         return obj
-    if obj.lower() == 'none':
+    if obj.lower() == PROGRESS_NONE:
         return null_progress.NullProgress()
-    if obj.lower == 'jupyter':
+    if obj.lower() == PROGRESS_NOTEBOOK:
         return jupyter_progress.JupyterProgress(verbosity=verbose)
-    if obj.lower == 'stdout' or obj.lower == 'console' or obj.lower == 'print':
+    if obj.lower() == PROGRESS_STDOUT or obj.lower == 'console' or obj.lower == 'print':
         return stdout_progress.PrintProgress(verbosity=verbose)
     raise Exception("Don't understand object {}".format(obj))
 
 
 def get_default_progress():
-    if notebook:
-        try:
-            import jupyter_progress
-            return jupyter_progress.JupyterProgress(verbosity=verbose)
-        except Exception as ex:
-            print('Unable to use JupyterProgress! An exception was raised:\n{}'.format(ex))
-    import stdout_progress
-    return stdout_progress.PrintProgress()
-
+    return factory(default_progress)
